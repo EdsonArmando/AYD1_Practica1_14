a13b29d69051b56a3c6367bf7948ccbd80d31c8c
velascoAndrea <velandreas@gmail.com>
2021-06-07 12:33:41 -0600

    subirmerge

---
+++ src/app/components/top-bar/top-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css']
+})
+export class TopBarComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+ dATOS(){
+    this.router.navigateByUrl('/DatosEstudiante');
+  }
+}
